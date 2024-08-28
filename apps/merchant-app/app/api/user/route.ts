d085705ac0350dbb17ddcc7f98a54728320cea8b
Naveed-23 <naveedbh23@gmail.com>
2024-08-28 13:33:53 +0530

    route

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from "next/server"
-import prisma from "@repo/db/client"
+// import { NextResponse } from "next/server"
+// import prisma from "@repo/db/client"
 
 // export const GET = async () => {
 //     await prisma.user.create({
